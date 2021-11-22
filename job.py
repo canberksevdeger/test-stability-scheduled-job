4728c6ae9d0be1617706e450b6b5684b21dbe56b
Canberk Sevdeger <canberk@amity.co>
2021-11-22 19:46:24 +0700

    for pr demo

--- job.py
+++ job.py
@@ -4,7 +4,6 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=9)
 def scheduled_job():
-    print('This job is run every weekday at 9am.')
     set_test_stability()
 
 sched.start()
